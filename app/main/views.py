fdf81a0c891618b61d736c3a013bf92c7089eb20
Kate <you@example.com>
2020-10-02 00:09:42 +0300

    views tamplates

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app import app
+from .requests import search_images
+
 
 
 
--- app/requests.py
+++ app/requests.py
@@ -11,13 +11,13 @@ base_url = app.config["API_BASE_URL"]
 # image_results=[]
 
 
-def search_images(photo_type):
-    search_image_url = 'https://pixabay.com/api/?key={}&q={}&image_type=photo').format(api_key,photo_type)
+def search_images(name):
+    search_image_url = 'https://pixabay.com/api/?key={}&q={}&image_type=photo').format(api_key,name)
     
 
     with urllib.request.urlopen(search_image_url) as url:
         search_image_data = url.read()
-        search_image_response = json.loads(search_movie_data)
+        search_image_response = json.loads(search_image_data)
 
         search_image_results = None
 
